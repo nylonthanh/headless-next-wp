a2df949dc519d2b25b86b3704377b0c14f08ab5d
Thanh Thao <thanhthao@Thanhs-MacBook-Pro.local>
2022-10-07 23:08:21 -0400

    add slug template

--- pages/index.js
+++ pages/index.js
@@ -40,49 +40,11 @@ export async function getStaticProps() {
   })
 
   const json = await rest.json();
-
+  
   return {
     props: {
       post: json.data.posts
     }
   }
 
-}
-
-export async function xgetStaticPaths() {
-
-  const res = await fetch('http://localhost:8888/graphql/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        query: `
-        query AllPostsQuery {
-          posts {
-            nodes {
-              slug
-              content
-              title
-              featuredImage {
-                node {
-                  sourceUrl
-                }
-              }
-            }
-          }
-        }
-        `
-      })
-    });
-
-  const json = await res.json();
-
-  console.log('json get static path: ', json);
-
-  const posts = json?.data?.posts?.nodes;
-
-  const paths = posts?.map(post => ({
-    params: { slug: post.slug },
-  }));
-
-  return { paths, fallback: false};
 }
